87dbba05c0ee135a1ef75b09b1962b1f5851efc5
Walter Livingston <wblivingston18@gmail.com>
2023-03-24 16:11:13 -0500

    branch cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,99 +1,30 @@
-#include "lucerna/lucerna.h"
+#include "SDL3/SDL.h"
+#include <iostream>
 
 int main(){
-    Window win("Lucerna", 1080, 720);
-
-    if(win.init()){
-        // Any OpenGL code must go in this if statement
-
-        float positions[16] = {
-            -50.0f, -50.0f, 0.0f, 0.0f,
-             50.0f, -50.0f, 1.0f, 0.0f,
-             50.0f,  50.0f, 1.0f, 1.0f,
-            -50.0f,  50.0f, 0.0f, 1.0f
-        };
-
-        unsigned int indices[6] = {
-            0, 1, 2,
-            2, 3, 0
-        };
-
-        GLCall(glEnable(GL_BLEND));
-        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-        VertexArray va;
-        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-        BufferLayout layout;
-        layout.Push<float>(2);
-        layout.Push<float>(2);
-        va.AddBuffer(vb, layout);
-
-        IndexBuffer ib(indices, 6);
-
-        glm::mat4 proj = glm::ortho(0.0f, (float)(win.getWidth()), 0.0f, (float)(win.getHeight()), -1.0f, 1.0f);
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
-
-        Shader shader("shaders/default.glsl");
-        shader.Bind();
-        shader.SetUniformMat4f("u_MVP", proj);
-
-        Texture tex("assets/textures/lamp.jpg");
-        tex.Bind();
-        shader.SetUniform1i("u_Texture", 0);
-
-        va.Unbind();
-        vb.Unbind();
-        ib.Unbind();
-        shader.Unbind();
-
-        Renderer renderer;
-
-        ImGui::CreateContext();
-        ImGui_ImplGlfw_InitForOpenGL(win.getWindow(), true);
-        ImGui_ImplOpenGL3_Init("#version 150");
-        ImGui::StyleColorsDark();
-
-        glm::vec3 translationA(200,200,0);
-        glm::vec3 translationB(400,200,0);
-
-        while(win.closed()){
-            renderer.Clear();
-            // Draw everything here
-            ImGui_ImplOpenGL3_NewFrame();
-            ImGui_ImplGlfw_NewFrame();
-            ImGui::NewFrame();
-
-            {
-                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-                glm::mat4 mvp = proj * view * model;
-                shader.Bind();
-                shader.SetUniformMat4f("u_MVP", mvp);
-
-                renderer.Draw(va, ib, shader);
-            }
-
-            {
-                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-                glm::mat4 mvp = proj * view * model;
-                shader.Bind();
-                shader.SetUniformMat4f("u_MVP", mvp);
-
-                renderer.Draw(va, ib, shader);
-            }
-
-            {
-                ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, win.getWidth());
-                ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, win.getWidth());
-                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            }
-
-            ImGui::Render();
-            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-            
-            win.update();
-        }
-        ImGui_ImplOpenGL3_Shutdown();
-        ImGui_ImplGlfw_Shutdown();
-        ImGui::DestroyContext();
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "Failed to initialize the SDL2 library\n";
+        return -1;
     }
+
+    // SDL_Window *window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 680, 480, 0);
+
+    // if(!window)
+    // {
+    //     std::cout << "Failed to create window\n";
+    //     return -1;
+    // }
+
+    // SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+
+    // if(!window_surface)
+    // {
+    //     std::cout << "Failed to get the surface from the window\n";
+    //     return -1;
+    // }
+
+    // SDL_UpdateWindowSurface(window);
+
+    // SDL_Delay(5000);
 }
